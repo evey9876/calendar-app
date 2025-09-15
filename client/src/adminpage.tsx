0f38ea9dedeb49741a0fa0be14beb03ad3a710e7
evey9876 <169172378+evey9876@users.noreply.github.com>
2025-09-15 21:47:00 +0100

    Upload client/src/adminpage.tsx

---
+++ client/src/adminpage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AdminPage() {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <p>Here you can manage all calendars and events.</p>
+    </div>
+  );
+}
