2f7a9a4d7a4d55b27e8272f3e4db9dd6ed4c5251
evey9876 <169172378+evey9876@users.noreply.github.com>
2025-09-15 21:46:59 +0100

    Upload client/src/UserPage.tsx

---
+++ client/src/UserPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function UserPage() {
+  return (
+    <div>
+      <h1>User Calendar</h1>
+      <p>Here you can view and manage your own events.</p>
+    </div>
+  );
+}
